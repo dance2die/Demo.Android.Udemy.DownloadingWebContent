9d4a4b2c4ff4f8c009ebd868e02a1503f91b7f5c
dance2die <dontbothermewithspam@gmail.com>
2016-03-29 07:29:24 -0400

    download internet web page async.

--- app/src/main/java/com/dance2die/demoandroidudemydownloadingwebcontent/MainActivity.java
+++ app/src/main/java/com/dance2die/demoandroidudemydownloadingwebcontent/MainActivity.java
@@ -6,17 +6,49 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
         @Override
-        protected String doInBackground(String... params) {
-            return null;
+        protected String doInBackground(String... urls) {
+//            Log.i("URL", urls[0]);
+//            return "Done";
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+                while (data != -1){
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                    Log.i("result...: ", result);
+                }
+
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "Failed";
+            }
         }
     }
 
@@ -35,6 +67,18 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        DownloadTask downloadTask = new DownloadTask();
+        String result = "";
+        try {
+            result = downloadTask.execute("http://www.google.com").get();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = "Failed...";
+        }
+
+        Log.i("Result", result);
     }
 
     @Override
